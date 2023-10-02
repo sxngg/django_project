6f588a779ad114f1543b90bf6527f1365da5bb14
sxngg <santiagogarciagarzon.8@gmail.com>
2023-10-01 21:59:35 -0500

    url default config

--- univalle_loans/urls.py
+++ univalle_loans/urls.py
@@ -5,6 +5,7 @@ from univalluno.views import GenerarMultaView, grafico_prestamos_por_deporte, gr
 
 urlpatterns = [
     path("reporte/", include("univalluno.urls")),
+    path('', admin.site.urls),
     path('admin/', admin.site.urls),
     path('generar_multa/', GenerarMultaView.as_view(), name='generar_multa'),
     path('grafico_prestamo_por_deporte', grafico_prestamos_por_deporte),
